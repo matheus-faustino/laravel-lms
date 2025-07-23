635f8fb92b10f616394b9abd5f2bf3e3cb55e314
matheus-faustino <matheus.melo.faus@gmail.com>
2025-07-23 22:50:58 +0000

    Create repository provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\BaseService;
+use App\Services\Interfaces\BaseServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(BaseServiceInterface::class, BaseService::class);
     }
 
     /**
