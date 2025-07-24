c1e666eb6f6ec48c49fd719ddcb048987e64550b
matheus-faustino <matheus.melo.faus@gmail.com>
2025-07-24 22:52:36 +0000

    Add admin and student users to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('12345678'),
+            'role' => User::ROLE_ADMIN,
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Test Student',
+            'email' => 'student@email.com',
+            'password' => Hash::make('12345678'),
+            'role' => User::ROLE_STUDENT,
         ]);
 
         $this->call([
